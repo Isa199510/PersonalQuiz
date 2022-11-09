1adceeb16926424a16b7ae24e9f280e241160424
Isa199510 <isa.arbuhov@mail.ru>
2022-11-09 18:46:19 +0600

    setup

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -13,12 +13,7 @@ class ResultViewController: UIViewController {
     @IBOutlet var definitionLabel: UILabel!
     
     var results: [Answer] = []
-    var counts: [AnimalType: Int] = [:]
-    
-    // 1. Передать массив с ответами на экран с результатами
-    // 2. Определить наиболее часто встречающийся тип животного
-    // 3. Отобразить результаты в соотвствии с этим животным
-    // 4. Избавиться от кнопки возврата назад на экране результатов
+    var animals: [AnimalType: Int] = [:]
 
     override func viewDidLoad() {
         
@@ -28,7 +23,7 @@ class ResultViewController: UIViewController {
     }
  
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
-        dismiss(animated: true)
+        navigationController?.dismiss(animated: true)
     }
     
 }
@@ -36,15 +31,13 @@ class ResultViewController: UIViewController {
 extension ResultViewController {
     func updateResult() {
         for result in results {
-            counts[result.type] = (counts[result.type] ?? 0) + 1
+            animals[result.type] = (animals[result.type] ?? 0) + 1
         }
         
-        let maxAnimal = counts.max { $0.value < $1.value }
+        let maxCountAnimal = animals.max { $0.value < $1.value }
+        guard let animal = maxCountAnimal?.key else { return }
         
-        guard let animal = maxAnimal?.key.rawValue,
-              let defination = maxAnimal?.key.definition else { return }
-        
-        animalLabel.text = "Вы - \(animal)"
-        definitionLabel.text = defination
+        animalLabel.text = "Вы - \(animal.rawValue)"
+        definitionLabel.text = animal.definition
     }
 }
